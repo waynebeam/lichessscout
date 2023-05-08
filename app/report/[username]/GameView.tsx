68cc094a0b04ba8c073cfecf8bb60989eccb752e
waynebeam <waynejcbeam@me.com>
2023-05-07 21:59:20 -0400

    First 5 opening moves toggleable

--- app/report/[username]/GameView.tsx
+++ app/report/[username]/GameView.tsx
@@ -11,18 +11,24 @@ interface GameViewParams {
 
 export default function GameView({ game }: GameViewParams) {
     const [pgnVisible, setPgnVisible] = useState(false);
+    const [openingMovesVisible, setOpeningMovesVisible] = useState(false);
     const whiteTitle = game.players.white.user.title ? game.players.white.user.title + ' ' : '';
     const blackTitle = game.players.black.user.title ? game.players.black.user.title + ' ' : '';
     const whitePlayer = game.players.white.user.name;
     const blackPlayer = game.players.black.user.name;
     return (
-        <div className={styles.game} onClick={() => setPgnVisible(!pgnVisible)}>
+        <div className={styles.game}>
             <p className={styles.gameInfo}>
                 <Link className={styles.link} href={`/report/${whitePlayer}`}>{whiteTitle}{whitePlayer} ({game.players.white.rating}) </Link>
                 vs. <Link className={styles.link} href={`/report/${blackPlayer}`}>{blackTitle}{blackPlayer} ({game.players.black.rating})</Link>
                 {!game.winner ? " 1/2-1/2" : game.winner === "white" ? " 1-0" : " 0-1"}</p>
-            <p className={styles.openingInfo}>{game.opening ? game.opening.name : game.id}</p>
-            <div className={styles.pgnToggle}>
+            <div onClick={() => setOpeningMovesVisible(!openingMovesVisible)}>
+                <p className={styles.openingInfo}>{game.opening ? game.opening.name : game.id}</p>
+                {
+                    openingMovesVisible ? game.moves.split(' ', 10).join(' ') : null
+                }
+            </div>
+            <div className={styles.pgnToggle} onClick={() => setPgnVisible(!pgnVisible)}>
                 {pgnVisible ? <p>PGN <br />  {game.pgn}</p> : <p>View PGN</p>}
             </div>
         </div>
