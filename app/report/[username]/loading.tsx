6f5888e97206c73104af082d7e4547a43703c9da
waynebeam <waynejcbeam@me.com>
2023-05-07 22:10:42 -0400

    cycling loading phrases

--- app/report/[username]/loading.tsx
+++ app/report/[username]/loading.tsx
@@ -1,3 +1,18 @@
+'use client';
+import { useState, useEffect } from "react";
+
+
 export default function Loading() {
-    return <div><h1>...Loading games...</h1></div>
+    const loadingPhrases = ['loading games', 'passing pawns', 'en passanting', 'checking mate', 'queening',
+        'pinning and winning', 'texting Magnus', 'stocking fish', 'scouting'];
+    const [index, setIndex] = useState(Math.floor(Math.random() * loadingPhrases.length));
+
+    useEffect(function () {
+        const timeout = setTimeout(function () {
+            setIndex(Math.floor(Math.random() * loadingPhrases.length));
+        }, 2000);
+        return () => clearTimeout(timeout);
+    })
+
+    return <div><h1>...{loadingPhrases[index]}...</h1></div>
 }
